63b1b49f990d24a93f4c3ca1f870da95dfad3a5f
jnszk <hi.jk.lm.23@gmail.com>
2020-02-24 23:17:32 +0900

    update shell script

--- run_jaqket_baseline_sample.sh
+++ run_jaqket_baseline_sample.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -x
 
-DDIR=../data_20200116/
+DDIR=../data/
+OUTDIR=output_dir
 
-python jaqket_baseline.py  \
+TRAIN=train_questions.json
+DEV=dev1_questions.json
+TEST=dev2_questions.json
+ENTITY=all_entities.json.gz
+
+python ../JAQKET_baseline/jaqket_baseline.py  \
   --data_dir   ${DDIR} \
   --model_name_or_path bert-base-japanese-whole-word-masking \
-  --task_name jpquiz \
+  --task_name jaqket \
+  --entities_fname ${ENTITY} \
+  --train_fname $TRAIN} \
+  --dev_fname   ${DEV} \
+  --test_fname  ${TEST} \
   --output_dir ${OUTDIR} \
   --train_num_options 4 \
   --do_train \
@@ -14,31 +24,19 @@ python jaqket_baseline.py  \
   --per_gpu_train_batch_size 1 \
   --gradient_accumulation_steps 8 \
   --num_train_epochs 5 \
-  --logging_steps 50 \
-  --save_steps 1000
+  --logging_steps 10 \
+  --save_steps 1000 \
 
-DEV=dev_QBIK.jsonl
-TEST=test_QBIK.jsonl
-python run_multiple_choice.py  \
+#--init_global_step 1000 \
+#--overwrite_output_dir
+
+python ../JAQKET_baseline/jaqket_baseline.py \
   --data_dir   ${DDIR} \
   --dev_fname  ${DEV}  \
   --test_fname ${TEST} \
-  --task_name jpquiz \
+  --task_name jaqket \
   --model_name_or_path ${OUTDIR} \
   --eval_num_options 20 \
-  --per_gpu_eval_batch_size 8 \
-  --do_test \
-  --do_eval
-
-DEV=dev_CAPR.jsonl
-TEST=test_CAPR.jsonl
-python run_multiple_choice.py  \
-  --data_dir   ${DDIR} \
-  --dev_fname  ${DEV}  \
-  --test_fname ${TEST} \
-  --task_name jpquiz \
-  --model_name_or_path ${OUTDIR} \
-  --eval_num_options 20 \
-  --per_gpu_eval_batch_size 8 \
+  --per_gpu_eval_batch_size 4 \
   --do_test \
   --do_eval
