2a9347341c4f760b7bc5f40a497e44a94943aaea
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2021-10-11 10:13:09 +0900

    Update data URLs in Amazon S3

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -7,10 +7,10 @@ if [ ! -d ${DATA_DIR} ]; then
   mkdir ${DATA_DIR}
 fi
 
-wget -nc https://jaqket.s3-ap-northeast-1.amazonaws.com/data/train_questions.json -P ${DATA_DIR}
-wget -nc https://jaqket.s3-ap-northeast-1.amazonaws.com/data/dev1_questions.json -P ${DATA_DIR}
-wget -nc https://jaqket.s3-ap-northeast-1.amazonaws.com/data/dev2_questions.json -P ${DATA_DIR}
-wget -nc https://jaqket.s3-ap-northeast-1.amazonaws.com/data/candidate_entities.json.gz -P ${DATA_DIR}
+wget -nc https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/train_questions.json -P ${DATA_DIR}
+wget -nc https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/dev1_questions.json -P ${DATA_DIR}
+wget -nc https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/dev2_questions.json -P ${DATA_DIR}
+wget -nc https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_01/all_entities.json.gz -P ${DATA_DIR}
 wget -nc https://www.nlp.ecei.tohoku.ac.jp/projects/AIP-LB/static/aio_leaderboard.json -P ${DATA_DIR}
 
 WORK_DIR="../working_dir/"
